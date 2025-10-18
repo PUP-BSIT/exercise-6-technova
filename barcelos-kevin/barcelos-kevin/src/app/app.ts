b2c6415e249a71b395f4b8489c80c93e81d465b2
barcelos-kevin <barceloskevinjoseph@gmail.com>
2025-10-18 20:41:14 +0800

    Add my book collection manager

--- barcelos-kevin/barcelos-kevin/src/app/app.ts
+++ barcelos-kevin/barcelos-kevin/src/app/app.ts
@@ -1,12 +1,61 @@
-import { Component, signal } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { Component, inject } from '@angular/core';
+import { FormBuilder, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
+import { CommonModule } from '@angular/common';
+
+interface Book {
+  id: number;
+  title: string;
+  author: string;
+  genre: string;
+  isRead: boolean;
+  rating: number;
+}
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [ReactiveFormsModule, CommonModule],
   templateUrl: './app.html',
   styleUrl: './app.scss'
 })
 export class App {
-  protected readonly title = signal('barcelos-kevin');
+  private formBuilder = inject(FormBuilder);
+  
+  bookForm: FormGroup;
+  books: Book[] = [];
+  nextId = 1;
+  
+  constructor() {
+    this.bookForm = this.formBuilder.group({
+      title: ['', { validators: [Validators.required] }],
+      author: ['', { validators: [Validators.required] }],
+      genre: ['', { validators: [Validators.required] }],
+      isRead: [false],
+      rating: [1, { validators: [
+                        Validators.required, 
+                        Validators.min(1), 
+                        Validators.max(5)
+              ] 
+      }]
+    });
+  }
+  
+  addBook() {
+    if (this.bookForm.valid) {
+      const newBook: Book = {
+        id: this.nextId++,
+        title: this.bookForm.value.title,
+        author: this.bookForm.value.author,
+        genre: this.bookForm.value.genre,
+        isRead: this.bookForm.value.isRead,
+        rating: this.bookForm.value.rating
+      };
+      
+      this.books.push(newBook);
+      this.bookForm.reset();
+    }
+  }
+  
+  removeBook(id: number) {
+    this.books = this.books.filter(book => book.id !== id);
+  }
 }
