cfb7f2776d53344b2dd2ff7715c643c7db2737f8
gomez-ashleyhermione <hermionegomez49@gmail.com>
2025-10-19 16:11:33 +0800

    feat: Implement song form with validation and song list display

--- gomez-ashleyhermione/songs-app/src/app/app.ts
+++ gomez-ashleyhermione/songs-app/src/app/app.ts
@@ -1,12 +1,13 @@
-import { Component, signal } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { Component } from '@angular/core';
+import { SongFormComponent } from './song-form/song-form';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  standalone: true,
+  imports: [SongFormComponent],
   templateUrl: './app.html',
   styleUrl: './app.scss'
 })
 export class App {
-  protected readonly title = signal('songs-app');
+  title = 'songs-app';
 }
