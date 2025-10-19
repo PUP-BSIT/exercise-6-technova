2c3fa309d83bdfb99e51ca6825ba7e84fc2284cd
gomez-ashleyhermione <hermionegomez49@gmail.com>
2025-10-19 16:04:37 +0800

    feat: initialize songs-app with structure and components

---
+++ gomez-ashleyhermione/songs-app/src/app/song-form/song-form.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-song-form',
+  imports: [],
+  templateUrl: './song-form.html',
+  styleUrl: './song-form.scss'
+})
+export class SongForm {
+
+}
