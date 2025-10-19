1bf4bfaa8772fe08027eb69fb24f92d39c1c693b
uy-angelicajoy <angelicajoyuy16@gmail.com>
2025-10-19 22:03:20 +0800

    refactor(transaction-tracker): adjust the import because it exceed the line limit

--- uy-angelicajoy/src/app/transaction-tracker/transaction-tracker.ts
+++ uy-angelicajoy/src/app/transaction-tracker/transaction-tracker.ts
@@ -1,6 +1,11 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { ReactiveFormsModule, FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { 
+  ReactiveFormsModule, 
+  FormBuilder, 
+  FormGroup, 
+  Validators 
+} from '@angular/forms';
 
 interface Transaction {
   itemName: string;
